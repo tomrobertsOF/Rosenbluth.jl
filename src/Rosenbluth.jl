ee6ed06a614cad3d84afcab036686712c837593d
Tom Roberts <38854624+tomrobertsOF@users.noreply.github.com>
2024-10-11 13:37:00 +0100

    fix

--- src/Rosenbluth.jl
+++ src/Rosenbluth.jl
@@ -204,7 +204,7 @@ function flattour!(::Type{T}, max_size::Int, weights, samples, bin_function::Fun
     enrichment_stack = Vector{Tuple{T,Float64}}()
     push!(enrichment_stack, (T(), 1.0))
 
-    started_tours = weights[bin_function(first(enrichment_stack))...] + 1
+    started_tours = weights[bin_function(T())...] + 1
 
     while !isempty(enrichment_stack)
         model, weight = pop!(enrichment_stack)
